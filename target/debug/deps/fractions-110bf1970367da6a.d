8527f7c733981f737d494a16e5fdc75f85a765e3
Jalen Troyer <jalentroyer@yahoo.com>
2024-10-16 14:31:23 -0400

    First commit

---
+++ target/debug/deps/fractions-110bf1970367da6a.d
@@ -0,0 +1,5 @@
+C:\Users\jalen\Documents\programming\rust\fractions\target\debug\deps\fractions-110bf1970367da6a.exe: src/main.rs
+
+C:\Users\jalen\Documents\programming\rust\fractions\target\debug\deps\fractions-110bf1970367da6a.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/fractions.d
@@ -0,0 +1,1 @@
+C:\Users\jalen\Documents\programming\rust\fractions\target\debug\fractions.exe: C:\Users\jalen\Documents\programming\rust\fractions\src\main.rs
